816c9ac70c6a343617262e2db11903a2f044acbd
G00410024 <g00410024@atu.ie>
2023-01-26 12:13:20 +0000

    First loop complete

--- src/main/java/ie/atu/week1/ArrayDemo.java
+++ src/main/java/ie/atu/week1/ArrayDemo.java
@@ -2,6 +2,15 @@ package ie.atu.week1;
 
 public class ArrayDemo {
     public static void main(String[] args) {
-    }
 
+        int[] firstArray;
+        firstArray = new int[10];
+
+        for(int i = 0; i <= 9; i++) {
+            int arrayItem = 100;
+            arrayItem *= i+1;
+            firstArray[i] = arrayItem;
+            System.out.println(i + " - Array item: " + firstArray[i]);
+        }
+    }
 }
